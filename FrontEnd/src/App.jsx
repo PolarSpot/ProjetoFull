ff805afa59b48ab4f29568f0d5f9689fd77907c5
Thomas de Almeida <thomaseduardAlmeida8@gmail.com>
2023-06-02 13:26:01 -0300

    Functions Add, Delete and Edit

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -19,35 +19,6 @@ export function App() {
 
       <div className={style.contactList}>
         <Header/>
-
-        <div className={style.contacts}>
-          <h1 className={style.letter} style={{ backgroundColor: "#07847E" }}> M </h1>
-            <Contato
-              avatar="https://randomuser.me/api/portraits/men/73.jpg"
-              name="Marcus Stephens"
-              phone="(489) 330-2385"
-            />
-
-            <Contato
-              avatar="https://randomuser.me/api/portraits/women/84.jpg"
-              name="Melissa Williams"
-              phone="(945) 804-6390"
-            />
-
-          <h1 className={style.letter} style={{ backgroundColor: "#0088B3" }}> R </h1>
-            <Contato
-              avatar="https://randomuser.me/api/portraits/men/6.jpg"
-              name="Roland Howard"
-              phone="(797) 346-1781"
-            />
-
-          <h1 className={style.letter} style={{ backgroundColor: "#9A00B3" }}> S </h1>
-            <Contato
-              avatar="https://randomuser.me/api/portraits/women/2.jpg"
-              name="Shelly Hopkins"
-              phone="(915) 909-2168"
-            />
-        </div>
       </div>
     </div>
 
